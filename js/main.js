1770a952b289d1242ff1bdaf1b19d29777b2079a
Destiny Goodrich <Msdestiny00@gamil.com>
2016-08-10 19:01:39 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var mapCanvas = document.getElementById("map");
+var mapOptions = {
+    center: new google.maps.LatLng(42.328279, -83.048025), zoom: 10
+}
+var map = new google.maps.Map(mapCanvas, mapOptions);
